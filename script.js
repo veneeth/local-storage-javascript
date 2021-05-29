85daed6ac2d2156f4f23ebb5c0c759f91dae2597
veneeth <gveneeth@gmail.com>
2021-05-29 22:13:34 +0530

    Updated File

---
+++ script.js
@@ -0,0 +1,46 @@
+const submit = document.querySelector('.comment-submit')
+const commentList = document.querySelector('.comments')
+const commentInput = document.querySelector('.comment-input')
+
+function template(data) {
+	commentList.insertAdjacentHTML(
+		'beforeend',
+		`
+            <div class="comment flex item-start justify-start">
+                <img
+                    class="comment-avatar"
+                    src="${data.avatar}"
+                />
+                <div class="flex-1">
+                    <h3 class="comment-author">${data.author}</h3>
+                    <div class="comment-body">${data.comment}</div>
+                </div>
+            </div>
+                
+                `,
+	)
+}
+
+function appendComment(event) {
+	const data = {
+		avatar:
+			'https://secure.gravatar.com/avatar/d1f5ca0d7e625f334c5186e112b77ebd',
+		comment: commentInput.value,
+		author: 'Veneeth',
+	}
+
+	event.preventDefault()
+
+	// if cooment text area value is empty exit
+	if (commentInput.value.length < 1) return
+
+	// if nt we continue
+	template(data)
+
+	// Clear text area once submited
+	commentInput.value = ''
+
+	//Local Storage
+}
+
+submit.addEventListener('click', appendComment, false)
